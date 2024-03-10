48dde1d442f6ad667c97e65c844d99a371d17cbc
mdyssr <mhmmdyssraamr@gmail.com>
2024-03-10 15:11:48 +0200

    edit config/date

--- tina/config.ts
+++ tina/config.ts
@@ -35,7 +35,8 @@ export default defineConfig({
         defaultItem: () => {
           return {
             title: "New Post",
-            date: new Date().toLocaleDateString("en"),
+            // date: new Date().toLocaleDateString("en"),
+            date: new Date(),
             draft: false,
           };
         },
